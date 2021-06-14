42bb3380e118decd46530d78b5a0b11bbc87aae8
Ben Hopkins <bjhops17@gmail.com>
2021-06-14 11:44:34 -0700

    added ability to retain constructor proporty

--- index.js
+++ index.js
@@ -18,25 +18,16 @@ function Cat(name){
 
 }
 
-// Subtype can use constructor properties
 
-// const kitten = new Animal("Cat", "Orange")
-// kitten.eat()
-
-
-// alternative instance Subtype can  use prototype but unsure about properties
-
-Cat.prototype={
-    constructor: Cat
-}
 
 Cat.prototype = Object.create(Animal.prototype)
+Cat.prototype.constructor = Cat
 
 
 const kitten = new Cat("fluff")
 
 // const kitten = Object.create(Animal.prototype)
-console.log(kitten.name)
+console.log(kitten.constructor)
 
 
 
